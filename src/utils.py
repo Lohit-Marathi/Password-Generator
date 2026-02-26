274b288fd5d0afc1021413941c224bfdc7326a3e
Lohit Marathi <marathilohit5@gmail.com>
2026-02-26 22:29:57 +0530

    Add utils.py file

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,27 @@ UPPERCASE = string.ascii_uppercase           # A-Z
 LOWERCASE = string.ascii_lowercase           # a-z
 DIGITS = string.digits                       # 0-9
 SYMBOLS = "!@#$%^&*()-_=+[]{}|;:,.<>?"       # special characters
+
+
+def get_char_pool(use_upper= True, use_symbols=True, use_digits=True):
+    """
+       Build the character pool based on user preferences.
+       
+       Args:
+          use_upper : Include uppercase letters
+          use_symbols : Include symbols
+          use_digits : Include digits
+          
+       Returns:
+          str : Combined pool of characters
+    """
+    pool = LOWERCASE  # always include lowercase letters
+
+    if use_upper:
+        pool += UPPERCASE
+    if use_symbols:
+        pool += SYMBOLS
+    if use_digits:
+        pool += DIGITS
+
+    return pool
