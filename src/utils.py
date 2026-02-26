5c30bbcca76d4f45710fe0799240f3bd73104d83
Lohit Marathi <marathilohit5@gmail.com>
2026-02-26 19:26:03 +0530

    Add initial files

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+"""Utility constants and helper functions"""
+
+
+import string
+
+# Character sets used for password generation
+UPPERCASE = string.ascii_uppercase           # A-Z
+LOWERCASE = string.ascii_lowercase           # a-z
+DIGITS = string.digits                       # 0-9
+SYMBOLS = "!@#$%^&*()-_=+[]{}|;:,.<>?"       # special characters
